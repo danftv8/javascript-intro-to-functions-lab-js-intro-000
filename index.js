121a3a66996a75b139bdd4dcf55dee689dd2b26b
Daniel Mullaney <danftv@gmail.com>
2021-01-10 21:56:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase()) {
   return ("I can\'t hear you!") }
-  if (string.toLowerCase()) {
+  else if (string.toLowerCase()) {
   return (" YES INDEED!") }
-  if (string("I love you, Grandma.")) {
+  else if (string("I love you, Grandma.")) {
   return ("I love you, too.") }
 }
