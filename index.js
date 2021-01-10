fc8d4797acc58e3e006d700caf27ec7f7d37d4e9
Daniel Mullaney <danftv@gmail.com>
2021-01-10 21:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
     return ("I can\'t hear you!")
 } if (string.toUpperCase()) {
   return ("YES INDEED!")
-} if ("I love you, Grandma.") {
+} if (string("I love you, Grandma.")) {
   return ("I love you, too.")
 }
 }
